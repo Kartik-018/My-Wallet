3c49528deac2badc3922abf947b0ed2ec9e05d9c
KARTIK DHANDHALA <kartikpaliwal991@gmail.com>
2024-06-10 12:00:20 +0530

    My-wallet for one user

---
+++ ExpenseTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        //the application db context instance will be created through dependency injection  so  update program.cs file
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        //through this constructor we have pass our dbprovider whether it is mysql or sql server and corresponding db connection string
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ ExpenseTracker/Models/Transaction.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+
+        [Range(1,int.MaxValue,ErrorMessage ="Please select the category.")]
+        public int CategoryId { get; set; }//It will create foreign key as cascase mode so when we delete from here it will also be deleleted from category table
+        public Category? Category { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be More than zero")]
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; }= DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleIcon 
+        {
+            get { return Category == null ? "" : Category.Icon + " " + Category.Title; }
+        }
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C0");
+            }
+        }
+    }
+}
